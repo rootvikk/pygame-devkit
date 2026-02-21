84263139832e0878841275b74039b034fe622616
Viraj Dhamnaskar <virajdhamnaskar7@gmail.com>
2026-02-21 23:23:36 +0530

    added more shapes

--- draw.py
+++ draw.py
@@ -16,10 +16,15 @@ while(running):
     screen.fill("WHITE")
 
     # -- Basic Shapes -- 
-    pygame.draw.rect(screen, ("RED"), (100, 100, 200, 100))
+    pygame.draw.rect(screen, "RED", (100, 100, 200, 100))
     # rect - (surface, color, (x, y, width, height))
 
-    pygame.draw.circle(screen, ("BLUE"), (400, 100), 50)
+    pygame.draw.circle(screen, "BLUE", (400, 100), 50)
+    # circle - (surface, color, (x,y), radius)
+
+    pygame.draw.line(screen, "BLACK", (100, 300), (500, 300), 5)
+    # line - (surface, color, (starting - x,y) (ending - x,y), width)
     
     pygame.display.flip()
+
 pygame.quit()
