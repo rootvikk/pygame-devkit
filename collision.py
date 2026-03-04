ba7e06708ae4a1ae1fa629ada40fd402f6db2841
Viraj Dhamnaskar <virajdhamnaskar7@gmail.com>
2026-03-04 18:52:22 +0530

    add delta time movement support

--- collision.py
+++ collision.py
@@ -3,8 +3,9 @@ import pygame
 pygame.init()
 
 window = pygame.display.set_mode((800,400))
+clock = pygame.time.Clock()
 
-speed = 1
+speed = 400
 
 player = pygame.Rect(50, 50, 100, 60)
 wall = pygame.Rect(700, 50, 30, 300)
@@ -12,6 +13,7 @@ wall = pygame.Rect(700, 50, 30, 300)
 running = True
 
 while(running):
+    dt = clock.tick (60) / 1000
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             running = False
@@ -19,13 +21,13 @@ while(running):
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_w]:
-        player.y -= speed
+        player.y -= speed * dt
     if keys[pygame.K_a]:
-        player.x -= speed
+        player.x -= speed * dt
     if keys[pygame.K_s]:
-        player.y += speed
+        player.y += speed * dt
     if keys[pygame.K_d]:
-        player.x += speed
+        player.x += speed * dt
 
     if player.colliderect(wall):
         color = "BLUE"
--- movement.py
+++ movement.py
@@ -4,17 +4,19 @@ pygame.init()
 
 window = pygame.display.set_mode((800,600))
 
+clock = pygame.time.Clock()
+
 # initial coords (gameObject spawns at - x,y)
 x = 300
 y = 200
 
-# movement speed
-speed = 1
-
+# movement speed (pixels/second)
+speed = 400
 
 running = True
 
 while(running):
+    dt = clock.tick (60) / 1000
     for z in pygame.event.get():
         if z.type == pygame.QUIT:
             running = False
@@ -23,13 +25,13 @@ while(running):
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_w]:
-        y -= speed
+        y -= speed * dt
     if keys[pygame.K_a]:
-        x -= speed
+        x -= speed * dt
     if keys[pygame.K_s]:
-        y += speed
+        y += speed * dt
     if keys[pygame.K_d]:
-        x += speed
+        x += speed * dt
 
     window.fill("WHITE")
 
